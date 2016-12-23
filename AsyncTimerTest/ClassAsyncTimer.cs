171ee978f9a2fde303005c7fd958611c5d31d203
dc-shi <daochen.shi@gmail.com>
2016-12-23 18:07:23 +0800

    Created a task.

---
+++ AsyncTimerTest/ClassAsyncTimer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTimerTest
+{
+    class ClassAsyncTimer : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation.
+        public event PropertyChangedEventHandler PropertyChanged;
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region Properties
+
+        int interval = 100;
+        bool isEnabled;
+        string status;
+        bool isInLoop = true;
+        int tickCount = 0;
+
+        /// <summary>
+        /// The trigger interval of this timer.
+        /// Note: if you set this to a very small number, the program might be hanged.
+        /// </summary>
+        public int Interval
+        {
+            get
+            {
+                return interval;
+            }
+
+            set
+            {
+                interval = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// This timer is enabled or not.
+        /// </summary>
+        public bool IsEnabled
+        {
+            get
+            {
+                return isEnabled;
+            }
+
+            set
+            {
+                isEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The status string of this timer.
+        /// </summary>
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This EventHandler would trigger after waiting interval.
+        /// </summary>
+        public event EventHandler Tick;
+
+        /// <summary>
+        /// Start in async method.
+        /// This method use Task.Delay.
+        /// </summary>
+        async void StartAsync()
+        {
+            Status = "Started in Async method";
+            while (isInLoop)
+            {
+                if (IsEnabled)
+                {
+                    Tick(this, EventArgs.Empty);
+                    Status = string.Format("Tick #{0}", ++tickCount);
+                }
+
+                /// This requires .Net 4.5
+                await Task.Delay(Interval);
+            }
+
+            Status = "Stopped";
+        }
+
+        /// <summary>
+        /// Start this timer.
+        /// </summary>
+        public void Start()
+        {
+            Status = "Starting";
+            isInLoop = true;
+            IsEnabled = true;
+            StartAsync();
+        }
+
+        /// <summary>
+        /// Stop this timer.
+        /// </summary>
+        public void Stop()
+        {
+            isInLoop = false;
+            Status = "Stopping...";
+        }
+    }
+}
---
+++ AsyncTimerTest/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncTimerTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window ,INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation.
+        public event PropertyChangedEventHandler PropertyChanged;
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cat.Tick += Cat_Tick;
+
+            textBox.DataContext = cat;
+            checkBox.DataContext = cat;
+            textBoxStatus.DataContext = cat;
+            textBoxIncrement.DataContext = this;
+        }
+
+        private void Cat_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value = progressBar.Minimum + (progressBar.Value + ProgressIncrementValue) % (progressBar.Maximum - progressBar.Minimum);
+        }
+
+        ClassAsyncTimer cat = new ClassAsyncTimer();
+
+        double progressIncrementValue = 0.41;
+
+        public double ProgressIncrementValue
+        {
+            get
+            {
+                return progressIncrementValue;
+            }
+
+            set
+            {
+                progressIncrementValue = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            cat.Start();
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            cat.Stop();
+        }
+
+        private void textBoxStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listBoxLog.Items.Add((sender as TextBox)?.Text);
+
+            /// http://stackoverflow.com/questions/2337822/wpf-listbox-scroll-to-end-automatically
+            if (VisualTreeHelper.GetChildrenCount(listBoxLog) > 0)
+            {
+                Border border = (Border)VisualTreeHelper.GetChild(listBoxLog, 0);
+                ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToBottom();
+            }
+        }
+    }
+}
